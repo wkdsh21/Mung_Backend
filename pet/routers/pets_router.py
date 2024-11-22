128b6f5bc3733a2fca671a314853a16e7b3d179a
wkdsh21 <wkdsh21@github.com>
2024-11-22 18:23:50 +0900

    user pull ,pets api test

--- pet/routers/pets_router.py
+++ pet/routers/pets_router.py
@@ -1,31 +1,49 @@
-from datetime import datetime
-from xmlrpc.client import DateTime
-
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
 from django.shortcuts import get_object_or_404
 from ninja import Router, Schema
+from ninja.security import django_auth
+
 from pet.schemas.pets_schema import PetCreateRequest, PetGetListResponse, PetUpdateRequest
 from pet.models import Pets
 
 router = Router()
 
-@router.get("/", response={200: list[PetGetListResponse]})
+@router.get("/", response={200: list[PetGetListResponse]}, auth=django_auth)
 def get_pets_list(request: HttpRequest) -> tuple[int, list[Pets]]:
     pets = Pets.objects.filter(user=request.user)
     return 200, [pet for pet in pets]
 
 
-@router.post("/")
+@router.post("/", auth=django_auth)
 def create_pet(request: HttpRequest, pet_request: PetCreateRequest) -> int:
-    Pets.objects.create(
-        **pet_request.dict()
+    obj=Pets.objects.create(
+        **pet_request.dict(),
+        user=request.user,
+        pet_id = request.user.pet_cnt + 1
     )
+    if obj:
+        request.user.pet_cnt += 1
+        request.user.save()
     return 201
 
 
-@router.put("/{id}")
+@router.put("/{id}", auth=django_auth)
 def update_pet(request: HttpRequest, pet_request: PetUpdateRequest, id: int) -> int:
-    Pets.objects.filter(user=request.user, pet_id=id).update(
+    pet = Pets.objects.filter(user=request.user, pet_id=id)
+    if pet.exists():
+        pet.update(
         **pet_request.dict()
-    )
+        )
+    else:
+        raise Http404
+    return 200
+
+@router.delete("/{id}", auth=django_auth)
+def delete_pet(request: HttpRequest, id: int) -> int:
+    pet = Pets.objects.filter(pet_id=id,user=request.user)
+    if pet.exists() and pet.delete()[0] == 1:
+        request.user.pet_cnt -= 1
+        request.user.save()
+    else:
+        raise Http404
     return 200
--- pet/schemas/pets_schema.py
+++ pet/schemas/pets_schema.py
@@ -1,39 +1,42 @@
 from datetime import datetime
+from typing import Literal, Annotated
 
+from annotated_types import Gt
 from ninja import Schema
 
 
 class PetCreateRequest(Schema):
-    animal_name: str
-    animal_img: str
-    animal_type: str
-    animal_species: str
+    name: str
+    profile_img: str
+    type: str
+    species: str
     is_neutering: bool
-    animal_born_in: datetime
-    animal_weight: float
+    birth_date: datetime
+    weight: Annotated[float, Gt(0)]
     need_diet: bool
 
     class Config:
         orm_mode = True
 
-
 class PetGetListResponse(Schema):
-    animal_name: str
-    animal_img: str
-    animal_type: str
-    animal_species: str
+    name: str
+    profile_img: str
+    type: str
+    species: str
     is_neutering: bool
-    animal_born_in: datetime
-    animal_weight: float
+    birth_date: datetime
+    need_diet: bool
+    weight: Annotated[float, Gt(0)]
 
     class Config:
         orm_mode = True
 
 class PetUpdateRequest(Schema):
-    animal_type: str
-    is_neutering: str
-    animal_born_in: datetime
-    animal_weight: float
+    type: str
+    is_neutering: bool
+    birth_date: datetime
+    need_diet: bool
+    weight: Annotated[float, Gt(0)]
 
     class Config:
         orm_mode = True
